30818173d5becc31f735a53bb98c97f696d1c6ae
Salman <salzahir18@gmail.com>
2025-03-11 18:27:58 -0500

    🖼️ Improved Header Layout – Wrapped title & logo for better styling

--- frontend-fiasco/src/Header.jsx
+++ frontend-fiasco/src/Header.jsx
@@ -4,8 +4,10 @@ import logo from "./assets/logo.jpg";
 function Header() {
     return (
         <header className="header">
-            <img src={logo} alt="Front End Fiasco Logo" />
-            <h2>Frontend Fiasco</h2>
+            <div className="title">
+                <h1>Frontend Fiasco</h1>
+                <img src={logo} alt="Front End Fiasco Logo" />
+            </div>
             <nav>
                 <Link to="/"><button>Home</button></Link>
                 <Link to="/products"><button>Products</button></Link>
