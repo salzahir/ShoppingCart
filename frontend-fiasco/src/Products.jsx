d0b8d671e634452122e6cd8695efeecfe3e90c14
Salman <salzahir18@gmail.com>
2025-03-11 18:21:53 -0500

    🛒 Refactored cart logic to store items & calculate total efficiently

--- frontend-fiasco/src/Products.jsx
+++ frontend-fiasco/src/Products.jsx
@@ -2,9 +2,9 @@ import parseProducts from "./api";
 import {fetchProducts} from "./api";
 import {useEffect, useState} from "react";
 
-
 function Products() {
     const [products, setProducts] = useState([])
+    const [cart, setCart] = useState([])
 
     useEffect(() => {
         fetchProducts().then(products => {
@@ -12,10 +12,19 @@ function Products() {
         })
     }, [])
 
+    function addToCart(product) {
+        setCart([...cart, product])
+    }
+
+    function calculateCartTotal() {
+        return cart.reduce((total, product) => total + product.price, 0);
+    }
+
     return (
         <div className="market">
             <h1>Products</h1>
             <p>Click on the products to add them to the cart</p>
+            <p>Total: $ {calculateCartTotal()}</p>
             <ul className="products">
                 {products.map(product => (
                     <li key={product.id} className="product">
@@ -24,9 +33,8 @@ function Products() {
                         <img src={product.image} alt={product.title} />
                         <p>Category: {product.category}</p>
                         <p>Description: {product.description}</p>
-                        <p>Price: {product.price}</p>
-                        <button>Add to cart</button>
-                    </li>
+                        <p>Price: $ {product.price}</p>
+                        <button onClick={() => addToCart(product)}>Add to cart</button>                    </li>
                 ))}
             </ul>
         </div>
